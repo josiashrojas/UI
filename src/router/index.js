abe868da223a630a24c7264519f824dedf317441
josiashrojas <josiasrojasramires@gmail.com>
2022-11-26 17:55:54 -0300

    Buscador para padre y pediatra, enlaces agregados al menu home

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import FichaView from '../views/FichaView.vue'
+import MenuPrincipalPadreView from '../views/MenuPrincipalPadreView.vue'
+import BuscarPacienteView from '../views/BuscarPacienteView.vue'
 
 Vue.use(VueRouter)
 
@@ -23,7 +25,18 @@ const router = new VueRouter({
       path: '/ficha',
       name: 'ficha',
       component: FichaView
-    }
+    },
+    {
+      path: '/padre',
+      name: 'padre',
+      component: MenuPrincipalPadreView
+    },
+    {
+      path: '/buscar',
+      name: 'buscar',
+      component: BuscarPacienteView
+    },
+
   ]
 })
 
---
+++ src/views/BuscarPacienteView.vue
@@ -0,0 +1,66 @@
+<script>
+
+export default {
+    data: () => ({
+      items: [
+        { text: 'Juanita Perez JR.'},
+        { text: 'Veronyre Sevel'},
+        { text: 'Stylos Rosewood'},
+        { text: 'Junipera Talonsnatch'},
+        { text: 'Angelo Ironmore'},
+        { text: 'Evodia Talonsnatch'},
+        { text: 'Matricara Watchwoods'},
+      ],
+      filterItem: [],
+      word: '',
+    }),
+    created:function() {
+      this.filterItem = [...this.items]
+    },
+    watch:{
+      word:function(){
+        this.filterItem = this.items.filter(item => item.text.toLowerCase().includes(this.word.toLowerCase()))
+      }
+    }
+  }
+</script>
+
+<template>
+  <main class="px-4">
+    <v-text-field
+        hide-details
+        append-icon="mdi-magnify"
+        single-line
+        full-width
+        onchange="filter"
+        v-model="word"
+        class="pb-8"
+      ></v-text-field>
+
+    <p v-if="filterItem.length == 0">No existen coincidencias</p>
+    <router-link 
+      v-for="(item,i) in filterItem"
+      :key="i"
+      to="/ficha"
+    >
+    <v-card
+      flat
+      tile
+      color="grey lighten-2"
+      class="pa-2 my-2 d-flex justify-space-between align-center"
+    >
+      <p>{{item.text}}</p>
+      <v-icon>mdi-weather-sunny</v-icon>
+    </v-card>
+  </router-link>
+  </main>
+</template>
+
+<style scoped>
+  p{
+    margin: 0;
+  }
+  a{
+    text-decoration: none;
+  }
+</style>
